ff0e0c9d028c15ad0c31040f08c0dc8c64b937f1
Jonathan <jfibush@gmail.com>
2018-12-27 15:38:09 +0200

    Adding virtual destructor

--- Expression.h
+++ Expression.h
@@ -1,9 +1,13 @@
 
 #ifndef UNTITLED5_EXPRESSION_H
 #define UNTITLED5_EXPRESSION_H
+
 class Expression {
 public:
     virtual double Calculate() = 0;
 
+    virtual ~Expression() {}
+
 };
+
 #endif //UNTITLED5_EXPRESSION_H
